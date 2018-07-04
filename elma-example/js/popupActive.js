4369a5e5d72d99c78ac9eb98d71a2be7117f5e40
Бабушкин Игорь <ibabushkin@elewise.local>
2018-07-04 14:19:43 +0400

    Исправляет недочеты

--- elma-example/js/popupActive.js
+++ elma-example/js/popupActive.js
@@ -5,10 +5,13 @@ var buttonCloseActivate = popupActivate.querySelector('.activate-form__button-cl
 var overlay = document.querySelector('.activate-form__overlay');
 var activationButton = document.querySelectorAll('.activation__button');
 var sectionActivate = document.querySelector('.activation');
+var body = document.getElementById('popup');
 
 var closePopup = function (element) {
   element.classList.add('hidden');
   overlay.classList.add('hidden');
+  body.style.overflow = 'auto';
+  overlay.style.overflow = 'hidden';
 };
 
 buttonCloseActivate.addEventListener('click', function () {
@@ -19,6 +22,8 @@ sectionActivate.addEventListener('click', function (evt) {
   for (var i = 0; i < activationButton.length; i++) {
     var target = evt.target;
     if (target === activationButton[i]) {
+      body.style.overflow = 'hidden';
+      overlay.style.overflow = 'scroll';
       popupActivate.classList.remove('hidden');
       overlay.classList.remove('hidden');
     }
