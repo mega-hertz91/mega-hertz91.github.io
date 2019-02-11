2c3ce3dccdf8ddb2195409781627ee46c2cb9c5f
mega-hertz91 <mega-hertz@bk.ru>
2019-02-11 14:55:12 +0400

    add project elmaday

--- kirov-dev-talks/js/form.js
+++ kirov-dev-talks/js/form.js
@@ -2,13 +2,36 @@
 
 var buttonForm = document.querySelector('.main-form__button');
 var inputNumberTel = document.querySelector('input[type=tel]');
+var inputs = document.querySelectorAll('input[type=text]');
+var mainForm = document.querySelector('.main-form');
 
 var checkTel = function () {
-    if (inputNumberTel.value.length > 12 || inputNumberTel.value.length < 5) {
-        inputNumberTel.setCustomValidity('Введите номер телефона в формате 79519993969');
-    } else {
-        inputNumberTel.setCustomValidity('');
+    var elemNumber = inputNumberTel.value.split('');
+
+    for (var i = 0; i < elemNumber.length; i++) {
+        if (elemNumber[i] < '0' || elemNumber > '9') {
+            inputNumberTel.setCustomValidity('Номер телефона должне содержать только цифры');
+        } else {
+            inputNumberTel.setCustomValidity('');
+        }
     }
 };
 
-buttonForm.addEventListener('click', checkTel);
+var chechBool = function () {
+  inputs.forEach(function (elem) {
+      if (elem.validity.valid === true) {
+          elem.style.boxShadow = 'inset 0 0 0 5px white';
+          elem.style.color = 'white';
+      } else {
+          elem.style.boxShadow = 'inset 0 0 0 5px #707070';
+          elem.style.color = '#707070';
+      }
+  });
+};
+
+var checkInputTel = function () {
+    checkTel();
+};
+
+buttonForm.addEventListener('click', checkInputTel);
+mainForm.addEventListener('keydown', chechBool);
